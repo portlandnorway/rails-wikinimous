09b225e8383d6e927aab012f847edf716df6c220
Dennis Neely <dennis.neely22@gmail.com>
2020-02-11 16:23:47 +0100

    added Article  routes, controller, and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+  Article.create(
+    title: Faker::Movies::StarWars.character,
+    content: Faker::Quote.yoda
+  )
+end
